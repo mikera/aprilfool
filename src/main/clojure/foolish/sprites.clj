f76811e1de815df610a974bca609e87feb42e892
Mike <mike@mikera.net>
2018-04-01 23:42:54 +0800

    Chika

--- src/main/clojure/foolish/sprites.clj
+++ src/main/clojure/foolish/sprites.clj
@@ -1,13 +1,19 @@
 (ns foolish.sprites)
 
 (def sprite-data 
-  
-  {"Henry" {:sprite 0
+  {
+   
+   "Henry" {:sprite 0
             :width 16
-            :height 16
-            
-            }
+            :height 16}
    
    "Lucy" {:sprite 1200
             :width 16
-            :height 16}})
+            :height 16}
+   
+   
+  "Chika" {:sprite 1202
+            :width 8
+            :height 8}         
+
+  })
